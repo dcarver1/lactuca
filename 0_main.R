f93567ab7bb4c1f6a977e74e9b47e369b002d840
dcarver1 <carver.dan1@gmail.com>
2024-07-12 15:42:30 -0700

    worked out the pca elements

--- 0_main.R
+++ 0_main.R
@@ -6,7 +6,7 @@
 
 pacman::p_load("terra", "sf", "dplyr", "readr", "stringr","raster", "ggplot2",
                "googlesheets4","googledrive", "tmap", "geodata", "rnaturalearth", "leaflet",
-               "tidyr")
+               "tidyr","FactoMineR","factoextra")
 
 
 # source functions --------------------------------------------------------
@@ -134,7 +134,7 @@ exVals2 <- exVals |>
 
 # the  degree C symbology causes some issues.... 
 write_csv(exVals,file = "outputs/ecogeographicDescription.csv")
-read_csv("outputs/ecogeographicDescription.csv")
+
 
 
 
@@ -211,15 +211,22 @@ for(i in seq_along(varList)){
 tbls <- exVals
 
 
-index <- 1
+index <- 15
 shortNames <- names(exVals[8:29])
 fullNames <- names(exVals2)[6:27]
-data <- gather(tbls, key = "Variable", value = "Value", -species )
+exportNames <- c(bioNames$`Current title`[1:19], "elevation","slope","aspect")
+data <- gather(tbls, key = "Variable", value = "Value", -species )|>
+  dplyr::mutate(factor = case_when(
+    species == "Lvir" ~ 1,
+    species == "Lser" ~ 2, 
+    species == "Lser, Lvir" ~ 3
+  ))
 
-createBoxPlot <- function(index, shortNames, fullNames, data){
+createBoxPlot <- function(index, shortNames, fullNames,exportNames, data){
   # select variable of interest 
   var <- shortNames[index]
   fullName <- fullNames[index]
+  exportName <- exportNames[index]
   # filter the dataset
   t3 <- data |> dplyr::filter(Variable == var)
   # reassign data 
@@ -230,10 +237,17 @@ createBoxPlot <- function(index, shortNames, fullNames, data){
   # labels 
   labels <-c("L. virosa", "L. serriola","L. serriola & L. virosa")
   
+  t3 |> 
+    group_by(species)|>
+    dplyr::summarise(mean = median(Value))
+
+  
   # generate the plot 
-  p1 <- ggplot(data = t3, aes(x = species, y = Value, color = species)) +
+  p1 <- ggplot(data =t3, aes(x = factor(species, levels = c( "Lvir","Lser", "Lser, Lvir" )),
+                             y = Value, 
+                             color = factor(species, levels = c( "Lvir","Lser", "Lser, Lvir" )))) +
     geom_boxplot()+
-    ggplot2::coord_flip()+
+    ggplot2::coord_flip() +
     ggplot2::xlab("") +
     scale_x_discrete(label = labels)+
     ggplot2::ylab("") +
@@ -243,10 +257,10 @@ createBoxPlot <- function(index, shortNames, fullNames, data){
           aspect.ratio=1/3)+
     facet_grid(. ~ tempvar) +
     theme(strip.background = element_rect(fill="#d1d9d8"),
-          strip.text = element_text(size=15, colour="black"))
+          strip.text = element_text(size=10, colour="black"))
   
   # export the image
-  ggsave(filename = paste0("outputs/boxplots/", var, "_2024_refined.png"),
+  ggsave(filename = paste0("outputs/boxplots/", exportName, "_2024_refined.png"),
          plot = p1, 
          units = "in", width = 6, height = 2)
   
@@ -255,6 +269,7 @@ createBoxPlot <- function(index, shortNames, fullNames, data){
 lapply(X = 1:22, FUN = createBoxPlot,
        shortNames = shortNames, 
        fullNames = fullNames,
+       exportNames = exportNames,
        data =   data)
 
 # # change species name 
@@ -327,236 +342,105 @@ ggplot(data = t3, aes(x = species, y = Value, color = species)) +
 # pca process  ------------------------------------------------------------
 
 
-### 2024 testing
-df <- d4[2:nrow(d4),]
 
-# PCA using all variables
-pca1 <- df %>%
-  dplyr::select("Annual mean temperature":wind) %>% 
-  FactoMineR::PCA(scale.unit = T, graph = F)
-
-pca1$ind$coord %>%
-  data.frame %>%
-  mutate(Species = df$species)|>  # tbls$species[intersect(rownames(df), tbls$id)]) %>%
-  ggplot(aes(x = Dim.1, y = Dim.2, colour = Species)) +
-  geom_point() +
-  geom_hline(yintercept = 0) +
-  geom_vline(xintercept = 0)
-
-outD <- "outputs/pca"
-
-# Eigen values
-fviz_screeplot(pca1, addlabels = TRUE, ylim = c(0, 50)) %>%
-  ggsave(filename = paste0(outD, "/eigen_vals.png"), plot = ., units = "in", width = 10, height = 8)
-
-# Control variable colors using their contributions
-fviz_pca_var(pca1, col.var = "cos2", # "contrib"
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE) %>%
-  ggsave(filename = paste0(outD, "/cos2.png"), plot = ., units = "in", width = 10, height = 8)
-
-# Contributions of variables to PC1
-fviz_contrib(pca1, choice = "var", axes = 1, top = 20) %>%
-  ggsave(filename = paste0(outD, "/contribution_axis1.png"), plot = ., units = "in", width = 10, height = 8)
-# Contributions of variables to PC2
-fviz_contrib(pca1, choice = "var", axes = 2, top = 20) %>%
-  ggsave(filename = paste0(outD, "/contribution_axis2.png"), plot = ., units = "in", width = 10, height = 8)
+## read in the dataset with geographic reference data attached 
+attData <- read_csv("outputs/ecogeographicDescription.csv")
+## drop some reference data 
+attData <- attData[,c(2,8:29)]
+## change the name of the species 
+attData <- attData |> 
+  dplyr::mutate(fullSpecies = case_when(
+    species == "Lvir" ~ "L. virosa",
+    species == "Lser" ~ "L. serriola", 
+    species == "Lser, Lvir" ~ "L. serriola & L. virosa"
+  ))
 
 
-# Biplot of individuals and variables
-fviz_pca_biplot(pca1, repel = TRUE)
-
-species <-unique(df$species)
-
-
-res.pca <- prcomp(df[,7:31],scale. = TRUE)
-
-fviz_pca_biplot(res.pca,
-                label = "var",
-                habillage = df$species, # tbls$speies[intersect(rownames(df), tbls$id)],
-                addEllipses = TRUE,
-                ellipse.level = 0.95,
-                repel = TRUE
-                ) %>%
-  ggsave(filename = paste0(outD, "/biplot_species.png"), 
-         plot = ., units = "in", width = 10, height = 8)
-
-# we don't have a type 
-# fviz_pca_biplot(pca1,
-#                 label = "var",
-#                 habillage = tbls$type[intersect(rownames(df), tbls$id)],
-#                 addEllipses = TRUE,
-#                 ellipse.level = 0.95) %>%
-#   ggsave(filename = paste0(outD, "/biplot_type.png"), plot = ., units = "in", width = 10, height = 8)
-
-fviz_pca_ind(res.pca,
-             label = "none", # hide individual labels
-             habillage =df$species,  # tbls$taxon_final[intersect(rownames(df), tbls$id)], # color by groups
-             palette = "Paired",
-             addEllipses = TRUE # Concentration ellipses
-) %>%
-  ggsave(filename = paste0(outD, "/individualpca.png"), plot = ., units = "in", width = 10, height = 8)
-
-
-clust_pca <- FactoMineR::HCPC(res = pca1, nb.clust = -1, graph = F)
-
-# Cluster interpretation
-cl1 <- round(clust_pca$desc.var$quanti$`1`, 2) %>% data.frame
-cl1$Variable <- rownames(cl1)
-cl1$Cluster <- 1
-cl2 <- round(clust_pca$desc.var$quanti$`2`, 2) %>% data.frame
-cl2$Variable <- rownames(cl2)
-cl2$Cluster <- 2
-cl3 <- round(clust_pca$desc.var$quanti$`3`, 2) %>% data.frame
-cl3$Variable <- rownames(cl3)
-cl3$Cluster <- 3
-cls <- rbind(cl1, cl2, cl3); rm(cl1, cl2, cl3)
-rownames(cls) <- 1:nrow(cls)
-
-g <- cls %>% filter(Cluster == 3) %>% ggplot(aes(x = reorder(Variable, v.test, FUN = median), y = v.test)) +
-  geom_bar(stat = "identity", position = position_dodge()) +
-  coord_flip() +
-  xlab("Variable") +
-  theme_minimal() +
-  theme(axis.text = element_text(size = 15),
-        axis.title = element_text(size = 18, face = "bold"),
-        legend.title = element_text(size = 15, face = "bold"),
-        legend.text =  element_text(size = 15))
-ggsave(filename = paste0(outD, "/interpretation_cluster3.png"), plot = g, units = "in", width = 8, height = 10)
-
-# Dendrogram
-fviz_dend(clust_pca, show_labels = FALSE)
-# Individuals facor map
-g <- fviz_cluster(clust_pca, geom = "point", main = "Factor map") +
-  geom_hline(yintercept = 0) +
-  geom_vline(xintercept = 0) +
-  theme_minimal()
-ggsave(filename = paste0(outD, "/clustering.png"), plot = g, units = "in", width = 10, height = 8)
-
-# cross_tab <- as.data.frame(table(tbls$taxon_final[intersect(rownames(df), tbls$id)], clust_pca$data.clust$clust))
-# cross_tab <- as.data.frame(table(tbls$taxon_final[intersect(rownames(df), tbls$id)], clust_pca$data.clust$clust)/rowSums(table(tbls$taxon_final[intersect(rownames(df), tbls$id)], clust_pca$data.clust$clust)))
-
-cross_tab <- as.data.frame(df$species, clust_pca$data.clust$clust)
-colnames(cross_tab) <- c("Species", "Cluster", "Freq")
-
-g <- cross_tab %>% ggplot(aes(x = factor(Cluster), y = Species, fill = Freq)) +
-  geom_tile(color = "white", size = 0.3) +
-  coord_equal() +
-  scale_fill_viridis(name = "Frequency", option = "C") +
-  theme(axis.text = element_text(size = 15),
-        axis.title = element_text(size = 18, face = "bold"),
-        legend.title = element_text(size = 15, face = "bold"),
-        legend.text =  element_text(size = 15))
-ggsave(filename = paste0(outD, "/pca/species_by_cluster.png"), plot = g, units = "in", width = 8, height = 10)
-
-clust_raw <- clValid::clValid(obj        = df %>% dplyr::select(bio_1:vapr) %>% scale(),
-                              nClust     = 2:10,
-                              clMethods  = c("hierarchical","kmeans","pam"), # "hierarchical"
-                              validation = c("internal", "stability")) # "internal"
-clValid::summary(clust_raw)
-cutree(tree = clust_raw@clusterObjs$hierarchical, k = 2) %>% length
-clust_raw@clusterObjs$pam$`10`$clustering %>% length
-
-hc.cut <- hcut(df %>% dplyr::select(bio_1:vapr) %>% scale(center = T, scale = T), k = 2, hc_method = "complete")
-
-fviz_silhouette(clust_raw@clusterObjs$pam$`10`, palette = "jco", ggtheme = theme_classic())
-fviz_silhouette(hc.cut, palette = "jco", ggtheme = theme_classic())
-
-
-fviz_nbclust(df %>% dplyr::select(bio_1:vapr) %>% scale(), kmeans, method = "gap_stat")
-
-
-table(cutree(tree = clust_raw@clusterObjs$hierarchical, k = 2),
-      clust_raw@clusterObjs$pam$`10`$clustering)
-
-
-clust_tsn <- Rtsne(df %>% dplyr::distinct() %>% scale(center = T, scale = T), dims = 2, perplexity = 50)
-plot(clust_tsn$Y, pch = 20)
-
-
-
-# 2022 data generation ----------------------------------------------------
-
-
-# clean input data --------------------------------------------------------
-d1 <- read_csv("data/analysis/lactuca2022.csv")%>%
-  dplyr::filter(!is.na(latitude))%>%
-  dms_dd(colname = "latitude")%>%
-  dms_dd(colname = "longitude")
-
-names(d1) <- c("Id","species","location","latitude","longitude","altitude")
-d1 <- d1 %>%
-  st_as_sf(coords = c( "longitude","latitude"), crs = "4326", remove = FALSE)
-
-# run extraction ----------------------------------------------------------
-# variable names 
-names <- read_csv("data/analysis/variableNames.csv")
-# output file 
-file <- "data/analysis/cropRaster.tif"
-if(!file.exists(file)){
-    # read in predictors 
-    r1 <- readRDS("data/analysis/parameters.RDS")
-    r2 <- terra::rast(r1)
-    names(r2) <- names$`Current title`
-    
-    
-    # # create point object 
-    sp1 <- sf::st_set_crs(d1,value = crs(r2))
-    
-    # buffer to get a little bit of boarder room and ensure the extraction 
-    sp2 <- st_buffer(x = sp1, dist = 50000) ## didn't seem to make a difference 
-    # crop raster 
-    r2 <- terra::crop(x = r2, y = sp2)
-    # write out raster
-    terra::writeRaster(r2, file)
-  
-    d2 <- terra::extract(r2, sp1) 
-    df <- dplyr::left_join(as.data.frame(d1), d2, by = c("Id" = "ID"))
-    write_csv(df, "outputs/ecoGeoData.csv")
-  }else{
-    r2 <- terra::rast(file)
-    # create point object 
-    sp1 <- sf::st_set_crs(d1,value = crs(r2))
-    # gather data 
-    d2 <- terra::extract(r2, sp1) 
-    df <- dplyr::left_join(as.data.frame(d1), d2, by = c("Id" = "ID"))
-  }
-
-
-# generate jitters --------------------------------------------------------
-tbls <- df[,c(1,2,8:33)]
-tbls <- tbls[!is.nan(tbls$`Annual mean temperature`),]
-varList <- names$`Current title`
-
-for(i in seq_along(varList)){
-  g <- tbls %>%
-    ggplot2::ggplot(aes(x = species, y =varList[i])) +
-    ggplot2::geom_jitter(colour = "gray", width = 0.20)+
-    ggplot2::xlab("") +
-    ggplot2::ylab("") +
-    ggplot2::coord_flip() +
-    ggplot2::theme_bw()+
-    ggplot2::theme(axis.text.y = ggplot2::element_text(face =  "bold.italic"))
-  # save files 
-  ggsave(filename = paste0("outputs/jitters/", varList[i], "_2024.png"), plot = g, units = "in", width = 7, height = 3)
-  rm(g)
-
+visualizeAndSave <- function(pcaplot, title, subtitle){
+  # more direct visualization of the plots 
+  p1 <- ggpubr::ggpar(pcaplot,
+                title = title,
+                subtitle = subtitle,
+                legend.title = "Taxon Group",
+                legend.position = "top",
+                ggtheme = theme_gray()
+                )
+  p1
+  #save result 
+  # ggexport(plotlist = p1, 
+  #          filename = paste0("outputs/pca/july2024exports/",subtitle,".png"))
+           # width = 800,
+           # height = 800,
+           # res = 300)
 }
 
+# for the PCA we want to drop the species name colum 
+names(attData)
+# generate the PCA 
+pca <- PCA(attData[,-c(1,24)],scale.unit = TRUE, graph = TRUE)
+
+# pca elispe with groups 
+pcaEllipse <- fviz_pca_ind(pca,
+             geom.ind = "point", # show points only (nbut not "text")
+             col.ind = attData$fullSpecies, # color by groups
+             palette = c(  "#00BA38","#F8766D","#619CFF"),
+             addEllipses = TRUE, # Concentration ellipses
+             mean.point = FALSE,
+             legend.title = "Taxon Groups"
+)
+visualizeAndSave(pcaplot = pcaEllipse,
+                 title = "Principal Component Analysis",
+                 subtitle = "Ellipse")
 
 
-# box plots 
-# Boxplots across species data per variable
+
+# Add confidence ellipses
+pcaEllipseConfidence <-fviz_pca_ind(pca, 
+             geom.ind = "point", 
+             col.ind = attData$fullSpecies, 
+             palette = c(  "#00BA38","#F8766D","#619CFF"),
+             addEllipses = TRUE, 
+             ellipse.type = "confidence",
+             mean.point = FALSE,
+             legend.title = "Groups"
+)
+visualizeAndSave(pcaplot = pcaEllipseConfidence,
+                 title = "Principal Component Analysis",
+                 subtitle = "Confidence Ellipse")
+# Convex hull
+convexHull <- fviz_pca_ind(pca,
+                           geom.ind = "point",
+             col.ind = attData$fullSpecies, # color by groups
+             palette =  c(  "#00BA38","#F8766D","#619CFF"),
+             addEllipses = TRUE,
+             ellipse.type = "convex",
+             legend.title = "Groups"
+)
+convexHull
+visualizeAndSave(pcaplot = convexHull,
+                 title = "Principal Component Analysis",
+                 subtitle = "Convex Hull Ellipse")
 
 
+# more direct visualization of the plots 
+ggpubr::ggpar(convexHull,
+              title = "Principal Component Analysis",
+              subtitle = "Convex hull ellipse",
+              legend.title = "Species Group",
+              legend.position = "top",
+              ggtheme = theme_gray()
+)
 
+# biplot with elipse 
+biplot <- fviz_pca_biplot(pca, 
+                col.ind = attData$fullSpecies,
+                palette =  c(  "#00BA38","#F8766D","#619CFF"),
+                addEllipses = TRUE, 
+                label = "var",
+                col.var = "black",
+                repel = TRUE,
+                legend.title = "Species Group") 
 
-  
-  
-
-# generate pca 
-
-
-
-# generate maps 
-
+visualizeAndSave(pcaplot = biplot,
+                 title = "Principal Component Analysis",
+                 subtitle = "Biplot")
